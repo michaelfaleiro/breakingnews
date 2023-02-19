10c4044da8fe269ed4ff6cf892fcd96415ada308
Michael Faleiro <michaelfaleiro@gmail.com>
2023-02-19 16:13:40 -0300

    Function search by title and user

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -3,6 +3,9 @@ import {
   createService,
   findAllService,
   topNewsService,
+  findByIdService,
+  searchByTitleService,
+  byUserService,
 } from "../services/news.service.js";
 
 export const create = async (req, res) => {
@@ -100,6 +103,83 @@ export const topNews = async (req, res) => {
         username: news.user.username,
         userAvatar: news.user.avatar,
       },
+    });
+  } catch (error) {
+    return res.status(500).send({ message: error.message });
+  }
+};
+
+export const findById = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const news = await findByIdService(id);
+
+    return res.send({
+      news: {
+        id: news._id,
+        title: news.title,
+        text: news.text,
+        banner: news.banner,
+        likes: news.likes,
+        comments: news.comments,
+        name: news.user.name,
+        username: news.user.username,
+        userAvatar: news.user.avatar,
+      },
+    });
+  } catch (error) {
+    return res.status(500).send({ message: error.message });
+  }
+};
+
+export const searchByTitle = async (req, res) => {
+  try {
+    const { title } = req.query;
+
+    const news = await searchByTitleService(title);
+
+    if (news.length === 0) {
+      return res
+        .status(400)
+        .send({ message: "There are no posts with this title." });
+    }
+
+    return res.send({
+      results: news.map((item) => ({
+        id: item._id,
+        title: item.title,
+        text: item.text,
+        banner: item.banner,
+        likes: item.likes,
+        comments: item.comments,
+        name: item.user.name,
+        username: item.user.username,
+        userAvatar: item.user.avatar,
+      })),
+    });
+  } catch (error) {
+    return res.status(500).send({ message: error.message });
+  }
+};
+
+export const byUser = async (req, res) => {
+  try {
+    const id = req.userId;
+
+    const news = await byUserService(id);
+    return res.send({
+      results: news.map((item) => ({
+        id: item._id,
+        title: item.title,
+        text: item.text,
+        banner: item.banner,
+        likes: item.likes,
+        comments: item.comments,
+        name: item.user.name,
+        username: item.user.username,
+        userAvatar: item.user.avatar,
+      })),
     });
   } catch (error) {
     return res.status(500).send({ message: error.message });
