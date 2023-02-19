a38e5a9f958170ff6d47b9e7130999728d5ff8ac
Michael Faleiro <michaelfaleiro@gmail.com>
2023-02-19 12:10:44 -0300

    Created Pagination

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -1,4 +1,9 @@
-import { createService, findAllService } from "../services/news.service.js";
+import {
+  countNews,
+  createService,
+  findAllService,
+  topNewsService,
+} from "../services/news.service.js";
 
 export const create = async (req, res) => {
   try {
@@ -22,11 +27,80 @@ export const create = async (req, res) => {
 
 export const findAll = async (req, res) => {
   try {
-    const news = await findAllService();
+    let { limit, offset } = req.query;
+
+    limit = Number(limit);
+    offset = Number(offset);
+
+    if (!limit) {
+      limit = 5;
+    }
+
+    if (!offset) {
+      offset = 0;
+    }
+
+    const news = await findAllService(offset, limit);
+    const total = await countNews();
+    const currentUrl = req.baseUrl;
+
+    const next = offset + limit;
+    const nextUrl =
+      next < total ? `${currentUrl}?limit=${limit}&offset=${next}` : null;
+
+    const previous = offset - limit < 0 ? null : offset - limit;
+    const previousUrl =
+      previous != null
+        ? `${currentUrl}?limit=${limit}&offset=${previous}`
+        : null;
+
     if (news.length === 0) {
       return res.status(400).send({ message: "There are no registered news" });
     }
-    return res.send(news);
+    return res.send({
+      nextUrl,
+      previousUrl,
+      limit,
+      offset,
+      total,
+      results: news.map((item) => ({
+        id: item._id,
+        title: item.title,
+        text: item.text,
+        banner: item.banner,
+        likes: item.likes,
+        comments: item.comments,
+        name: item.user.name,
+        username: item.user.username,
+        userAvatar: item.user.avatar,
+      })),
+    });
+  } catch (error) {
+    return res.status(500).send({ message: error.message });
+  }
+};
+
+export const topNews = async (req, res) => {
+  try {
+    const news = await topNewsService();
+
+    if (!news) {
+      return res.status(400).send({ message: "There is no registered news" });
+    }
+
+    res.send({
+      news: {
+        id: news._id,
+        title: news.title,
+        text: news.text,
+        banner: news.banner,
+        likes: news.likes,
+        comments: news.comments,
+        name: news.user.name,
+        username: news.user.username,
+        userAvatar: news.user.avatar,
+      },
+    });
   } catch (error) {
     return res.status(500).send({ message: error.message });
   }
